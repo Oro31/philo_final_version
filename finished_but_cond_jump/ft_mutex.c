b4410969ec62bc76ada93ab12c778a2bf2756e57
Oro31 <60878633+Oro31@users.noreply.github.com>
2021-11-15 15:17:05 +0100

    valgrind cond jump

---
+++ finished_but_cond_jump/ft_mutex.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_mutex.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rvalton <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/12 16:50:46 by rvalton           #+#    #+#             */
+/*   Updated: 2021/11/15 13:45:21 by rvalton          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosopher.h"
+
+void	ft_lock_vars_mutex(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->vars->mutex);
+	philo->vars->is_mutex_lock = 1;
+}
+
+void	ft_unlock_vars_mutex(t_philo *philo)
+{
+	philo->vars->is_mutex_lock = 0;
+	pthread_mutex_unlock(&philo->vars->mutex);
+}
+
+void	ft_lock_forks_mutex(t_philo *philo, int i)
+{
+	int	j;
+
+	j = (i + 1) % philo->vars->nb_philos;
+	pthread_mutex_lock(&philo->vars->forks[i].mutex);
+	philo->vars->forks[i].use = 1;
+	pthread_mutex_lock(&philo->vars->forks[j].mutex);
+	philo->vars->forks[j].use = 1;
+}
+
+void	ft_unlock_forks_mutex(t_philo *philo, int i)
+{
+	int	j;
+
+	j = (i + 1) % philo->vars->nb_philos;
+	philo->vars->forks[i].use = 0;
+	pthread_mutex_unlock(&philo->vars->forks[i].mutex);
+	philo->vars->forks[j].use = 0;
+	pthread_mutex_unlock(&philo->vars->forks[j].mutex);
+}
---
+++ finished_but_cond_jump/ft_thread.c
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_thread.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rvalton <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/09 12:08:17 by rvalton           #+#    #+#             */
+/*   Updated: 2021/11/15 15:12:54 by rvalton          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosopher.h"
+
+int	ft_is_philo_dead(struct timeval last_meal, t_philo *philo)
+{
+	struct timeval	t;
+	int				ret;
+
+	ret = gettimeofday(&t, NULL);
+	if (ft_timediff(last_meal, t) > philo->ttd)
+		return (1);
+	return (0);
+}
+
+void	ft_philo_eat(t_philo *philo, int i)
+{
+	ft_lock_forks_mutex(philo, i);
+	if (ft_is_philo_dead(philo->last_meal, philo))
+	{
+		ft_died(philo, i);
+		ft_unlock_vars_mutex(philo);
+	}
+	else
+	{
+		ft_eating(philo, i);
+		ft_satiated(philo);
+		ft_unlock_vars_mutex(philo);
+		ft_wait(philo->last_meal, philo->tte, philo);
+	}
+}
+
+void	ft_philo_sleep(t_philo *philo, int i)
+{
+	ft_lock_vars_mutex(philo);
+	if (philo->alive && philo->vars->play)
+	{
+		ft_sleeping(philo, i);
+		ft_unlock_forks_mutex(philo, i);
+		ft_unlock_vars_mutex(philo);
+		ft_wait(philo->t, philo->tts, philo);
+		ft_lock_vars_mutex(philo);
+		if (philo->alive && philo->vars->play)
+			ft_thinking(philo, i);
+		ft_unlock_vars_mutex(philo);
+	}
+	else
+		ft_unlock_vars_mutex(philo);
+}
+
+void	ft_play_philo(t_philo *philo, int i, int j)
+{
+	if (ft_is_philo_dead(philo->last_meal, philo))
+	{
+		ft_died(philo, i);
+		ft_unlock_vars_mutex(philo);
+	}
+	else if (philo->vars->nb_philos_satiated == philo->vars->nb_philos)
+	{
+		philo->alive = 0;
+		philo->vars->play = 0;
+		ft_unlock_vars_mutex(philo);
+	}
+	else if (philo->vars->forks[i].use == 0 && philo->vars->forks[j].use == 0)
+	{
+		ft_philo_eat(philo, i);
+		ft_philo_sleep(philo, i);
+	}
+	else
+		ft_unlock_vars_mutex(philo);
+}
+
+void	*ft_th_philo(void *p_data)
+{
+	t_philo	*philo;
+	int		i;
+	int		j;
+
+	philo = (t_philo *)p_data;
+	i = philo->id;
+	j = (i + 1) % philo->vars->nb_philos;
+	philo->ret = gettimeofday(&philo->last_meal, NULL);
+	philo->alive = 1;
+	ft_thinking(philo, i);
+	while (philo->alive)
+	{
+		ft_lock_vars_mutex(philo);
+		if (philo->vars->play == 0)
+		{
+			philo->alive = 0;
+			ft_unlock_vars_mutex(philo);
+		}
+		else
+			ft_play_philo(philo, i, j);
+		philo->ret = gettimeofday(&philo->t, NULL);
+		ft_wait(philo->t, 10, philo);
+	}
+	return (NULL);
+}
